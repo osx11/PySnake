cbfdfafef069530d8bf09718d8c12ac10da0ff96
osx11 <osx11@ya.ru>
2019-04-25 18:19:54 +0700

    gridder

--- src/gridder.py
+++ src/gridder.py
@@ -1,10 +1,18 @@
-def create_grid(canvas_width, canvas_height, grid_width, grid_height, pixel_side):
-    grid_coordinates = {}
-    x1 = 10
-    x2 = pixel_side+10
-    for i in range(grid_width):
-        coordinate = x1, canvas_height-30, x2, canvas_height-10
-        grid_coordinates[(0, i)] = coordinate
-        x1 += 20
-        x2 += 20
-    return grid_coordinates
+def create_grid(canvas_height, grid_width, grid_height):
+    l_grid_coordinates = {}
+
+    y1 = canvas_height-30
+    y2 = canvas_height-10
+
+    for y in range(grid_height):
+        x1 = 10
+        x2 = 30
+        for x in range(grid_width):
+            l_coordinate = x1, y1, x2, y2
+            l_grid_coordinates[(x, y)] = l_coordinate
+            x1 += 20
+            x2 += 20
+        y1 -= 20
+        y2 -= 20
+
+    return l_grid_coordinates
--- src/gui.py
+++ src/gui.py
@@ -2,9 +2,11 @@
 
 import keyboard
 import tkinter as tk
-from src import snake_logic as sl
 from time import sleep
 import threading
+
+from src import snake_logic as sl
+from src import gridder
 
 NAME = 'PySnake'
 WIDTH = 600
@@ -17,27 +19,8 @@ HEIGHT = 600
 # canvas.create_rectangle(grid_coordinates[(x, y)])
 #
 # высота поля, количество квадратов по x и y могут быть произвольными
-def create_grid(canvas_height, grid_width, grid_height):
-    l_grid_coordinates = {}
 
-    y1 = canvas_height-30
-    y2 = canvas_height-10
-
-    for y in range(grid_height):
-        x1 = 10
-        x2 = 30
-        for x in range(grid_width):
-            l_coordinate = x1, y1, x2, y2
-            l_grid_coordinates[(x, y)] = l_coordinate
-            x1 += 20
-            x2 += 20
-        y1 -= 20
-        y2 -= 20
-
-    return l_grid_coordinates
-
-
-grid_coordinates = create_grid(canvas_height=HEIGHT, grid_width=29, grid_height=29)
+grid_coordinates = gridder.create_grid(canvas_height=HEIGHT, grid_width=29, grid_height=29)
 
 
 def update_grid():
